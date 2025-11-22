ec22ae581cfe0dddd22cca27fb7f9cddb80f1a63
Michael <mixzelenin@yandex.ru>
2025-11-22 14:02:40 +0500

    add new function

--- bot.py
+++ bot.py
@@ -22,5 +22,19 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
-
+ 
+@bot.message_handler(func=lambda message: True)
+def check_messages(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    user_status = bot.get_chat_member(chat_id, user_id).status
+    
+    # Пропускаем администраторов
+    if user_status in ['administrator', 'creator']:
+        return
+    
+    # Проверяем наличие ссылок в сообщении
+    if message.text and ("https://" in message.text or "http://" in message.text):
+        bot.ban_chat_member(chat_id, user_id)
+        bot.reply_to(message, f"Пользователь @{message.from_user.username} был забанен за отправку ссылки.")
 bot.infinity_polling(none_stop=True)
